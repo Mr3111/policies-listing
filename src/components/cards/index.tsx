b592c9609b81b9a62df9e4eb7208a42bd2a347ea
Rishab Mishra <rishab.mishra@bfhl.in>
2022-03-26 18:46:56 +0530

    fixed key type issue

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Table, Tag } from 'antd';
 import {
     IProductId,
@@ -6,12 +6,11 @@ import {
 } from '../../modules/productComparator';
 import { Button } from '@components/button';
 import styled from 'styled-components';
-
-export type IKeys = IProductId[];
+import { Key } from 'antd/lib/table/interface';
 
 type CardProps = Pick<ProductComparatorProps, 'products'> & {
-    selectedRowKeys: IKeys;
-    onSelectChange: (keys: IKeys) => void;
+    selectedRowKeys: Key[];
+    onSelectChange: (keys: Key[]) => void;
     selectedProductId?: IProductId;
 };
 
@@ -26,59 +25,62 @@ export const Cards: React.FC<CardProps> = ({
     onSelectChange,
     selectedProductId,
 }) => {
-    const columns = [
-        {
-            title: 'Id',
-            dataIndex: 'id',
-            key: 'id',
-        },
-        {
-            title: 'Name',
-            dataIndex: 'name',
-            key: 'name',
-            render: (text: string) => <a>{text}</a>,
-        },
-        {
-            title: 'Price/ year',
-            dataIndex: 'totalCost',
-            key: 'price',
-        },
-        {
-            title: 'Doctor consultations',
-            dataIndex: 'doctorConsultations',
-            key: 'doctorConsultations',
-        },
-        {
-            title: 'AYUSH Treatment',
-            dataIndex: 'ayushTreatment',
-            key: 'ayushTreatment',
-        },
-        {
-            title: 'Type',
-            key: 'type',
-            dataIndex: 'isExternal',
-            render: (isExternal: boolean) => (
-                <Tag color={isExternal ? 'volcano' : 'green'}>
-                    {isExternal ? 'External' : 'Internal'}
-                </Tag>
-            ),
-        },
-        {
-            title: 'Action',
-            key: 'action',
-            render: (text: any, record: any) => (
-                <>
-                    {selectedProductId === record.id ? (
-                        <Button type="primary">Selected</Button>
-                    ) : (
-                        <Button disabled={record.isExternal}>
-                            Select Product
-                        </Button>
-                    )}
-                </>
-            ),
-        },
-    ];
+    const columns = useMemo(
+        () => [
+            {
+                title: 'Id',
+                dataIndex: 'id',
+                key: 'id',
+            },
+            {
+                title: 'Name',
+                dataIndex: 'name',
+                key: 'name',
+                render: (text: string) => <a>{text}</a>,
+            },
+            {
+                title: 'Price/ year',
+                dataIndex: 'totalCost',
+                key: 'price',
+            },
+            {
+                title: 'Doctor consultations',
+                dataIndex: 'doctorConsultations',
+                key: 'doctorConsultations',
+            },
+            {
+                title: 'AYUSH Treatment',
+                dataIndex: 'ayushTreatment',
+                key: 'ayushTreatment',
+            },
+            {
+                title: 'Type',
+                key: 'type',
+                dataIndex: 'isExternal',
+                render: (isExternal: boolean) => (
+                    <Tag color={isExternal ? 'volcano' : 'green'}>
+                        {isExternal ? 'External' : 'Internal'}
+                    </Tag>
+                ),
+            },
+            {
+                title: 'Action',
+                key: 'action',
+                render: (text: any, record: any) => (
+                    <>
+                        {selectedProductId === record.id ? (
+                            <Button type="primary">Selected</Button>
+                        ) : (
+                            <Button disabled={record.isExternal}>
+                                Select Product
+                            </Button>
+                        )}
+                    </>
+                ),
+            },
+        ],
+        [],
+    );
 
     const rowSelection = {
         selectedRowKeys,
